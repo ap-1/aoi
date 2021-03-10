c0d683deb11b0d5199f85e152f286046876e165b
EmperorParzival <29981946+EmperorParzival@users.noreply.github.com>
2021-03-10 17:51:38 -0500

    fix: Resolve consistency issues

--- aoi/cogs/general.py
+++ aoi/cogs/general.py
@@ -2,14 +2,14 @@ import discord
 from discord.ext import commands
 
 class General(commands.Cog, name="General Commands"):
-	def __init__(self, bot):
-		self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 	
-	@commands.command()
-	async def ping(self, ctx):
-		""" Find the latency from the bot to discord. """
+    @commands.command()
+    async def ping(self, ctx):
+        """ Find the latency from the bot to discord. """
 
-		await ctx.send(f"pong! ({self.bot.latency * 1000} ms)")
+        await ctx.send(f"pong! ({self.bot.latency * 1000} ms)")
 
 def setup(bot):
-	bot.add_cog(General(bot))
+    bot.add_cog(General(bot))
--- aoi/main.py
+++ aoi/main.py
@@ -13,43 +13,43 @@ activity = discord.Activity(activity=discord.ActivityType.listening, name="to yo
 @client.command()
 @commands.is_owner()
 async def load(ctx, *, name: str):
-	"""Load a cog."""
+    """Load a cog."""
 
-	try:
-		client.load_extension(f"aoi.cogs.{name}")
-	except commands.ExtensionNotFound:
-		await ctx.send(f"{name} doesn't exist")
-	except commands.ExtensionAlreadyLoaded:
-		await ctx.send(f"{name} is already loaded")
-	except commands.ExtensionFailed as err:
-		print(f"an error prevented {cog} from loading:\n{err}")
-	else:
-		await ctx.send(f"{name} has been loaded.")
+    try:
+        client.load_extension(f"aoi.cogs.{name}")
+    except commands.ExtensionNotFound:
+        await ctx.send(f"{name} doesn't exist")
+    except commands.ExtensionAlreadyLoaded:
+        await ctx.send(f"{name} is already loaded")
+    except commands.ExtensionFailed as err:
+        print(f"an error prevented {cog} from loading:\n{err}")
+    else:
+        await ctx.send(f"{name} has been loaded")
 
 @client.command()
 @commands.is_owner()
 async def unload(ctx, *, name: str):
-	"""Unload a cog."""
+    """Unload a cog."""
 
-	try:
-		client.unload_extension(f"aoi.cogs.{name}")
-	except commands.ExtensionNotFound:
-		await ctx.send(f"{name} doesn't exist")
-	except commands.ExtensionNotLoaded:
-		await ctx.send(f"{name} isn't loaded")
-	else:
-		await ctx.send(f"unloaded {name}")
+    try:
+        client.unload_extension(f"aoi.cogs.{name}")
+    except commands.ExtensionNotFound:
+        await ctx.send(f"{name} doesn't exist")
+    except commands.ExtensionNotLoaded:
+        await ctx.send(f"{name} isn't loaded")
+    else:
+        await ctx.send(f"unloaded {name}")
 
 @client.event
 async def on_ready():
-	await client.change_presence(status=discord.Status.dnd, activity=activity)
-	print(f"{client.user.name} loaded.")
+    await client.change_presence(status=discord.Status.dnd, activity=activity)
+    print(f"{client.user.name} loaded.")
 
-def init():
-	for cog in ["general"]:
-		try:
-			client.load_extension(f"aoi.cogs.{cog}")
-		except commands.ExtensionFailed as err:
-			print(f"an error prevented {cog} from loading:\n{err}")
+    def init():
+    for cog in ["general"]:
+        try:
+            client.load_extension(f"aoi.cogs.{cog}")
+        except commands.ExtensionFailed as err:
+            print(f"an error prevented {cog} from loading:\n{err}")
 
-	client.run(token, reconnect=True)
+    client.run(token, reconnect=True)
