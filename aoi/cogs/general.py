a3a9603fe34fe533c71dfb3a66e8f37700246c83
EmperorParzival <29981946+EmperorParzival@users.noreply.github.com>
2021-03-11 13:47:10 -0500

    feat: Convert ping to a slash command

--- aoi/cogs/general.py
+++ aoi/cogs/general.py
@@ -1,15 +1,21 @@
-import discord
 from discord.ext import commands
+from discord_slash import cog_ext
+
+guild_ids = [765588555010670654]
 
 class General(commands.Cog, name="General Commands"):
     def __init__(self, bot):
         self.bot = bot
-	
-    @commands.command()
+
+    @cog_ext.cog_slash(name="ping",
+                       description="Find the latency from the bot to discord.",
+                       guild_ids=guild_ids)
     async def ping(self, ctx):
-        """ Find the latency from the bot to discord. """
+        """Find the latency from the bot to discord."""
+        await ctx.respond()
 
         await ctx.send(f"pong! ({self.bot.latency * 1000} ms)")
+
 
 def setup(bot):
     bot.add_cog(General(bot))
