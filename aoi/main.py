1184d17e00d2b7e217d3e80a3c9e632fe843d89a
EmperorParzival <29981946+EmperorParzival@users.noreply.github.com>
2021-05-11 23:43:36 -0400

    feat: Prevent ghost ping alerts from being deleted

--- aoi/main.py
+++ aoi/main.py
@@ -18,22 +18,37 @@ activity = discord.Activity(type=discord.ActivityType.listening, name="you")
 client = commands.Bot(command_prefix=commands.when_mentioned_or('/'), intents=intents)
 slash = SlashCommand(client, sync_commands=True, sync_on_cog_reload=True)
 
+async def check_undeletable(messages):
+    for message in messages:
+        if len(message.embeds) != 1 or message.embeds[0].author.name != "Ghost Ping":
+            continue
+        
+        await message.channel.send(embed=message.embeds[0])
+
+@client.event
+async def on_message_delete(message):
+    await check_undeletable([ message ])
+
+@client.event
+async def on_bulk_message_delete(messages):
+    await check_undeletable(messages)
+
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.dnd, activity=activity)
     print(f"{client.user.name} loaded.")
 
 @slash.slash(name="load",
-             description="Load a cog.",
-             guild_ids=whitelisted_guilds,
-             options=[
+            description="Load a cog.",
+            guild_ids=whitelisted_guilds,
+            options=[
                 create_option(
                     name="cog",
                     description="the cog to load",
                     option_type=3,
                     required=True
                 )
-             ])
+            ])
 @is_owner
 async def load(ctx, name: str):
     await ctx.respond()
@@ -45,21 +60,21 @@ async def load(ctx, name: str):
     except commands.ExtensionAlreadyLoaded:
         await ctx.send(f"{name} is already loaded")
     except commands.ExtensionFailed as err:
-        print(f"an error prevented {cog} from loading:\n{err}")
+        print(f"an error prevented {name} from loading:\n{err}")
     else:
         await ctx.send(f"{name} has been loaded")
 
 @slash.slash(name="unload",
-             description="Unload a cog.",
-             guild_ids=whitelisted_guilds,
-             options=[
+            description="Unload a cog.",
+            guild_ids=whitelisted_guilds,
+            options=[
                 create_option(
                     name="cog",
                     description="the cog to unload",
                     option_type=3,
                     required=True
                 )
-             ])
+            ])
 @is_owner
 async def unload(ctx, name: str):
     await ctx.respond()
